8a299691272c82eabc969ad3d28915dd1684b14b
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2023-11-20 05:51:26 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Bruno Oliveira on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
