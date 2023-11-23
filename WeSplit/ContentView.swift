db1ed11c798cc6d0874874447898903f79210a18
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2023-11-23 05:50:15 -0300

    Hidding the keyboard and focused mode

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var totalPerPerson: Double {
@@ -32,6 +33,7 @@ struct ContentView: View {
                 Section("Check Amount and number of People:") {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BRL"))
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
                     Picker("Number Of People", selection: $numberOfPeople) {
                         ForEach(2..<100) {
@@ -53,6 +55,13 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Include WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
